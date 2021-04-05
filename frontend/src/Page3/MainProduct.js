7d88e28aa97e122d27865d33bd50a7da1983421a
Aditya Rai <adityarai2109@gmail.com>
2021-04-05 22:53:29 +0530

    ReactImageMagnify implemented

--- frontend/src/Page3/MainProduct.js
+++ frontend/src/Page3/MainProduct.js
@@ -6,23 +6,48 @@ import Cardarray from "../homePage/cardarray";
 import Navbar from "../homePage/navbar";
 import Footer from "../homePage/footer5";
 import MyFooter from "../homePage/myFooter";
+import ReactImageMagnify from "react-image-magnify";
 
 // myId = this.props.match.params.ida
 
 const MainProduct = ({ match }) => {
   let id = parseInt(match.params.ida); // isko int me convert krna tha
-  let category = String(match.params.category)
+  let category = String(match.params.category);
   return (
     <>
       <div>
         <Navbar />
         <div className="page3_content">
           <div className="product_image">
-            <img
+            {/* <img
               className="pimage"
-              src={Cardarray.find((x) => x.id === id && x.category === category).image}
+              src={
+                Cardarray.find((x) => x.id === id && x.category === category)
+                  .image
+              }
               alt="ProductImage"
-            ></img>
+            ></img> */}
+            <ReactImageMagnify
+              {...{
+                smallImage: {
+                  alt: "Wristwatch by Ted Baker London",
+                  isFluidWidth: true,
+                  src: Cardarray.find(
+                    (x) => x.id === id && x.category === category
+                  ).image,
+                  width: 200,
+                  height: 200,
+                },
+                largeImage: {
+                  src: Cardarray.find(
+                    (x) => x.id === id && x.category === category
+                  ).image,
+                  enlargedImagePosition: "beside",
+                  width: 1000,
+                  height: 1000,
+                },
+              }}
+            />
           </div>
           <div className="partition"></div>
           <div className="product_desc">
@@ -34,7 +59,6 @@ const MainProduct = ({ match }) => {
             </p>
             <div className="buttons1">
               <PurpleButton
-
                 line1={"₹ " + Cardarray.find((x) => x.id === id).price}
               />
               <PurpleButton line1="Add to Wishlist" />
