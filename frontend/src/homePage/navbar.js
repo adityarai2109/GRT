9a3f8aaf3053e92705783ebad867e5ed777e349e
Aditya Rai <adityarai2109@gmail.com>
2021-04-06 19:18:25 +0530

    Minor changes

--- frontend/src/homePage/navbar.js
+++ frontend/src/homePage/navbar.js
@@ -1,31 +1,31 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
-import iconLogo from '../images/group1.svg'
+import iconLogo from "../images/group1.svg";
 
 import "./navbar.css";
 
 export default function Navbar(props) {
-  
-  function handleSearch(e){
-   
+  function handleSearch(e) {
     props.setQuery(e.target.value);
-    
+    console.log(props.query);
   }
   return (
     <div className="navbar">
       <a className="logo" href="/">
-        <img style={{height:"8vh"}} src={iconLogo} alt="GRT"/>
-        <h4 style={{marginLeft:"2rem"}}>Ganga Ram Textiles</h4>
+        <img style={{ height: "8vh" }} src={iconLogo} alt="GRT" />
+        <h4 style={{ marginLeft: "2rem" }}>Ganga Ram Textiles</h4>
       </a>
       <div className="searchbar">
         <input
           className="inputnav"
           type="text"
           placeholder="Search for Clothes, Brands and more..."
-          onChange={e => handleSearch(e)}
-         
+          onChange={(e) => handleSearch(e)}
         />
-        <SearchIcon className="search"  style={{ cursor:"pointer",fontSize: "36px" }} />
+        <SearchIcon
+          className="search"
+          style={{ cursor: "pointer", fontSize: "36px" }}
+        />
       </div>
     </div>
   );
