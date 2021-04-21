016650bc164c159784bb2210da74cc9803e80108
ritik-07 <ritikr.0173@gmail.com>
2021-04-21 19:45:40 +0530

    dotenv fixed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import  express from 'express'
 import Cardarray from './data/cardarray.js'
+import dotenv from 'dotenv'
+
 const app = express();
+dotenv.config();
 
 const PORT = process.env.PORT || 5000
 
@@ -18,7 +21,5 @@ app.get("/api/products/:id", (req, res) => {
 });
 
   const MODE = process.env.NODE_ENV;
-  const a = process.env.MONGO_URI;
-  console.log(a)
 
 app.listen(PORT, console.log(`Server running in ${MODE} mode on ${PORT}`));
