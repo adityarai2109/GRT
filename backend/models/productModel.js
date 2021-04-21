ad9943dfd25b1b80b4df7609e35983e6290ab1e9
ritik-07 <ritikr.0173@gmail.com>
2021-04-21 20:08:45 +0530

    models

---
+++ backend/models/productModel.js
@@ -0,0 +1,16 @@
+
+import mongoose from 'mongoose'
+
+const productSchema = mongoose.Schema({
+
+ 
+
+ 
+
+
+})
+
+
+const Product = mongoose.model('product', productSchema)
+
+  export default Product
