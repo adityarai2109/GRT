ac70c24d464d083326b3b936c606d124f3e80055
ritik-07 <ritikr.0173@gmail.com>
2021-04-21 20:52:22 +0530

    product_model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,16 +1,55 @@
+import mongoose from "mongoose";
 
-import mongoose from 'mongoose'
-
+const eeviewSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  rating: { type: String, required: true },
+  comment: { type: String, required: true },
+});
 const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 
- 
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  reviews: [reviewSchema],
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
 
- 
+const Product = mongoose.model("product", productSchema);
 
-
-})
-
-
-const Product = mongoose.model('product', productSchema)
-
-  export default Product
+export default Product;
