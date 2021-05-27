9c0e5915c26226cb1b045f652dfb98eed0f3dd2c
unknown <keshav2608g@gmail.com>
2021-05-27 22:17:16 +0530

    Nav fix

--- frontend/src/homePage/AdminNav.js
+++ frontend/src/homePage/AdminNav.js
@@ -12,96 +12,107 @@ import Fade from "@material-ui/core/Fade";
 import { AdminContext } from "../context/AdminState";
 
 export default function AdminNav(props) {
-  const { signOut } = useContext(AdminContext);
-  const LightTooltip = withStyles((theme) => ({
-    tooltip: {
-      backgroundColor: theme.palette.common.white,
-      color: "rgba(0, 0, 0, 0.87)",
-      boxShadow: theme.shadows[10],
-      fontSize: 14,
-      fontFamily: "Poppins",
-    },
-  }))(Tooltip);
-  window.onscroll = function () {
-    doStick();
-  };
+    const { signOut } = useContext(AdminContext);
+    const LightTooltip = withStyles((theme) => ({
+        tooltip: {
+            backgroundColor: theme.palette.common.white,
+            color: "rgba(0, 0, 0, 0.87)",
+            boxShadow: theme.shadows[10],
+            fontSize: 14,
+            fontFamily: "Poppins",
+        },
+    }))(Tooltip);
+    window.onscroll = function () {
+        doStick();
+    };
 
-  const handleSignOut = () => {
-    signOut();
-  };
-  function doStick() {
-    const searchbar = document.getElementById("searchbar");
-    const navbar = document.getElementById("navbar").offsetHeight;
-    if (window.pageYOffset >= navbar) searchbar.classList.add("sticky");
-    else searchbar.classList.remove("sticky");
-  }
+    const handleSignOut = () => {
+        signOut();
+    };
+    function doStick() {
+        const searchbar = document.getElementById("searchbar");
+        const navbar = document.getElementById("navbar").offsetHeight;
+        if (window.pageYOffset >= navbar) searchbar.classList.add("sticky");
+        else searchbar.classList.remove("sticky");
+    }
 
-  return (
-    <div className="main_nav">
-      <div className="navbar">
-        <div id="navbar">
-          <Link to="/">
-            <div className="logo">
-              <img style={{ height: "8vh" }} src={iconLogo} alt="GRT" />
-              <h4 className="grt">Dashboard</h4>
+    return (
+        <div className="main_nav">
+            <div className="navbar">
+                <div id="navbar">
+                    <Link to="/">
+                        <div className="logo">
+                            <img
+                                style={{ height: "8vh" }}
+                                src={iconLogo}
+                                alt="GRT"
+                            />
+                            <h4 className="grt">Dashboard</h4>
+                        </div>
+                    </Link>
+                </div>
+
+                <form
+                    id="searchbar"
+                    className="searchbar"
+                    action={`/Dashboard`}
+                    method="GET"
+                >
+                    <input
+                        className="inputnav"
+                        id="inputnavid"
+                        type="text"
+                        placeholder={
+                            window.innerWidth > 768
+                                ? `Search for clothes, brands and more ...`
+                                : `Search for products ...`
+                        }
+                        name="q"
+                    />
+                    <div
+                        className="btn-search"
+                        style={{ borderRadius: "0px 3px 3px 0px" }}
+                    >
+                        <Button component={Link} type="submit">
+                            <img
+                                src={iSearch}
+                                className="search"
+                                style={{ cursor: "pointer", height: "4vh" }}
+                                alt="Search"
+                            />
+                        </Button>
+                    </div>
+                </form>
+
+                <div
+                    className="nr"
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-around",
+                    }}
+                >
+                    <LightTooltip
+                        TransitionComponent={Fade}
+                        TransitionProps={{ timeout: 600 }}
+                        title="Sign Out"
+                    >
+                        <Button onClick={handleSignOut}>
+                            <ExitToAppIcon
+                                fontSize="large"
+                                style={{ color: "white" }}
+                            />
+                        </Button>
+                    </LightTooltip>
+                    <Toggle
+                        className="nav_toggle"
+                        isDarkMode={props.isDarkMode}
+                        setIsDarkMode={props.setIsDarkMode}
+                    />
+                </div>
             </div>
-          </Link>
         </div>
-
-        <form action={`/Dashboard`} method="GET">
-        <div id="searchbar" className="searchbar">
-          <input
-            className="inputnav"
-            id="inputnavid"
-            type="text"
-            placeholder={
-              window.innerWidth > 768
-                ? `Search for clothes, brands and more ...`
-                : `Search for products ...`
-            }
-            name="q"
-          />
-          <div
-            className="btn-search"
-            style={{ borderRadius: "0px 3px 3px 0px" }}
-          >
-            <Button type="submit">
-              <img
-                src={iSearch}
-                className="search"
-                style={{ cursor: "pointer", height: "4vh" }}
-                alt="Search"
-              />
-            </Button>
-          </div>
-        </div>
-        </form>
-        <div
-          className="nr"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
-          }}
-        >
-          <LightTooltip
-            TransitionComponent={Fade}
-            TransitionProps={{ timeout: 600 }}
-            title="Sign Out"
-          >
-            <Button onClick={handleSignOut}>
-              <ExitToAppIcon fontSize="large" style={{ color: "white" }} />
-            </Button>
-          </LightTooltip>
-          <Toggle
-            className="nav_toggle"
-            isDarkMode={props.isDarkMode}
-            setIsDarkMode={props.setIsDarkMode}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 /*
 <SearchIcon
--- frontend/src/homePage/Navbar.js
+++ frontend/src/homePage/Navbar.js
@@ -7,69 +7,73 @@ import Toggle from "../CustomJS/Toggle";
 import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
+    window.onscroll = function () {
+        doStick();
+    };
 
-  window.onscroll = function () {
-    doStick();
-  };
+    function doStick() {
+        const searchbar = document.getElementById("searchbar");
+        const navbar = document.getElementById("navbar").offsetHeight;
+        // console.log(stick)
+        if (window.pageYOffset >= navbar) searchbar.classList.add("sticky");
+        else searchbar.classList.remove("sticky");
+    }
 
-  function doStick() {
-    const searchbar = document.getElementById("searchbar");
-    const navbar = document.getElementById("navbar").offsetHeight;
-    // console.log(stick)
-    if (window.pageYOffset >= navbar) searchbar.classList.add("sticky");
-    else searchbar.classList.remove("sticky");
-  }
+    return (
+        <div className="main_nav">
+            <div className="navbar">
+                <div id="navbar">
+                    <Link to="/">
+                        <div className="logo">
+                            <img
+                                style={{ height: "8vh" }}
+                                src={iconLogo}
+                                alt="GRT"
+                            />
+                            <h4 className="grt">Ganga Ram Textiles</h4>
+                        </div>
+                    </Link>
+                </div>
+                <form
+                    id="searchbar"
+                    className="searchbar"
+                    action={`/Suit`}
+                    method="GET"
+                >
+                    <input
+                        className="inputnav"
+                        id="inputnavid"
+                        type="text"
+                        placeholder={
+                            window.innerWidth > 768
+                                ? `Search for clothes, brands and more ...`
+                                : `Search for products ...`
+                        }
+                    />
 
-  return (
-    <div className="main_nav">
-      <div className="navbar">
-        <div id="navbar">
-          <Link to="/">
-            <div className="logo">
-              <img style={{ height: "8vh" }} src={iconLogo} alt="GRT" />
-              <h4 className="grt">Ganga Ram Textiles</h4>
+                    <div
+                        className="btn-search"
+                        style={{ borderRadius: "0px 3px 3px 0px" }}
+                    >
+                        <Button>
+                            <img
+                                src={iSearch}
+                                className="search"
+                                style={{ cursor: "pointer", height: "4vh" }}
+                                alt="Search"
+                            />
+                        </Button>
+                    </div>
+                </form>
+
+                <Toggle
+                    className="nav_toggle"
+                    isDarkMode={props.isDarkMode}
+                    setIsDarkMode={props.setIsDarkMode}
+                />
             </div>
-          </Link>
         </div>
-        <form action={`/Suit`} method="GET">
-        <div id="searchbar" className="searchbar">
-          <input
-            className="inputnav"
-            id="inputnavid"
-            type="text"
-            placeholder={
-              window.innerWidth > 768
-                ? `Search for clothes, brands and more ...`
-                : `Search for products ...`
-            }
-            name="q"
-          />
-
-          <div
-            className="btn-search"
-            style={{ borderRadius: "0px 3px 3px 0px" }}
-          >
-            <Button type="submit">
-              <img
-                src={iSearch}
-                className="search"
-                style={{ cursor: "pointer", height: "4vh" }}
-                alt="Search"
-              />
-            </Button>
-          </div>
-        </div>
-        </form>
-        
-
-        <Toggle
-          className="nav_toggle"
-          isDarkMode={props.isDarkMode}
-          setIsDarkMode={props.setIsDarkMode}
-        />
-      </div>
-    </div>
-  );
+    );
 }
 /*
 <SearchIcon
